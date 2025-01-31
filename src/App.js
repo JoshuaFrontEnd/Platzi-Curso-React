f6f4fbd4371aadfdd247797b40c8110c75e7c34e
Joshua <hola@joshua.cl>
2025-01-31 20:53:36 -0300

    Props y Atributos

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import { TodoItem } from './TodoItem';
 import { TodoList } from './TodoList';
 import { TodoSearch } from './TodoSearch';
 
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+];
+
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <>
+      <TodoCounter completed={16} total={25} />
 
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
-    </div>
+    </>
   );
 }
 
